0d7b89170f1b1b433aec0109d62e5ae5f2da29d4
Attah David <jocheattahdavid@gmail.com>
2020-10-12 16:42:29 +0100

    wip home page

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__container">
+                <div>Home</div>
+                <div>Featured Projects</div>
+                <div>Other projects</div>
+                <div>About</div>
+                <div>Contact</div>                
+            </div>            
+        </div>
+    )
+}
+
+export default Header
+
---
+++ src/components/Main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div className="main">
+      <div className="main__contents">
+        <div className="main__first">Hi, my name is</div>
+        <div className="main__name">Attah David Joche</div>
+        <div className="main__name main__name--work">I build things for the web</div>
+        <p className="main__paragraph">
+          I'm a design-minded software engineer in Abuja, NGR who specializes in <br></br>
+          building (and occasionally designing) exceptional high-quality <br></br>
+          websites and applications.
+        </p>
+        <button>Get in touch</button>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
