8b2d6002139fbcacf1b75a5a1bad8bb28d15660f
Attah David <jocheattahdavid@gmail.com>
2020-10-15 16:31:06 +0100

    to fix tags

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Project from "./Project";
-import { db } from "../services/firebase";
+import  db  from "../services/firebase";
 import { useEffect } from "react";
 
 function Featured() {
@@ -9,20 +9,35 @@ function Featured() {
   useEffect(() => {
     db
     .collection("posts")
-    .orderBy("timestamp", desc)
+    .orderBy("timestamp", "desc")
     .onSnapshot((snapshot) => {
       const postsData = snapshot.docs.map((doc)=>({
         id:doc.id,
         post:doc.data()
-      }))
-    })
+      }));
+      setPosts(postsData);
+    });
   }, []);
+
+
   return (
     <div className="featured" id="fProjects">
       <div className="featured___content">
         <h3 className="featured__heading">Featured</h3>
         <h3 className="featured__heading">Projects</h3>
-        <Project />
+        {posts.map(({id, post})=>(
+          <Project 
+          key={id}
+          id={id}
+          name={post.name}
+          info={post.info}
+          image={post.image}
+          tags={post.tags}
+          github={post.github}
+          weblink={post.weblink}
+
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,26 +3,21 @@ import { Link } from "react-router-dom";
 import GitHubIcon from "../assets/icons/github.svg";
 import webLink from "../assets/icons/weblink.svg";
 
-function Project() {
+function Project({id, name, image, info, tags, github, weblink}) {
   return (
     <div className="project">
       <div className="project__imageContainer">
         <img
-          src="https://github.com/JocheAttah/react_bmi_calculator_chart/raw/master/img/bmi.jpg"
+          src={image}
           alt="bmi screenshot"
           className="project__image"
         />
       </div>
       <div className="project__content">
         <p className="project__tag">Featured project</p>
-        <h3 className="project__name">Amazon Clone</h3>
+        <h3 className="project__name">{name}</h3>
         <div className="project__detailContainer">
-          <p className="project__detail">
-            I'm a design-minded software engineer in Abuja, NGR who specializes{" "}
-            <br />
-            in building (and occasionally designing) exceptional high-quality{" "}
-            <br />
-            websites and applications.
+          <p className="project__detail">{info}
           </p>
         </div>
         <div className="project__tag project__techs">
@@ -32,11 +27,11 @@ function Project() {
           <div className="tech">SCSS</div>
         </div>
         <div className="project__link">
-          <Link>
+          <Link to={github}>
             <img  className="project__link--github" src={GitHubIcon} alt="Github" />
           </Link>
 
-          <Link >
+          <Link to= {weblink}>
             <img className="project__link--live" src={webLink} alt="web link" />
           </Link>
         </div>
