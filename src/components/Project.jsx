93987bee2598f08cfe8d5ee6770270981391b7dc
Attah David <jocheattahdavid@gmail.com>
2020-10-16 00:14:55 +0100

    fixed external links

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,37 +3,43 @@ import { Link } from "react-router-dom";
 import GitHubIcon from "../assets/icons/github.svg";
 import webLink from "../assets/icons/weblink.svg";
 
-function Project({id, name, image, info, tags, github, weblink}) {
+function Project({ id, name, image, info, tags, github, weblink }) {
   return (
     <div className="project">
       <div className="project__imageContainer">
-        <img
-          src={image}
-          alt="bmi screenshot"
-          className="project__image"
-        />
+        <img src={image} alt="bmi screenshot" className="project__image" />
       </div>
       <div className="project__content">
         <p className="project__tag">Featured project</p>
         <h3 className="project__name">{name}</h3>
         <div className="project__detailContainer">
-          <p className="project__detail">{info}
-          </p>
+          <p className="project__detail">{info}</p>
         </div>
         <div className="project__tag project__techs">
-          <div className="tech">ReactJS</div>
-          <div className="tech">ChartJS</div>
+            {console.table(tags)}
+            {Object.keys(tags).forEach(tag =>(
+                <div>{tags[tag]}</div>
+            ))}
+            {/* {for (let tag in tags) {
+                <div>{tags[tag].value}</div>
+            }} */}
+
+          {/* <div className="tech">ChartJS</div>
           <div className="tech">JavaScript</div>
-          <div className="tech">SCSS</div>
+          <div className="tech">SCSS</div> */}
         </div>
         <div className="project__link">
-          <Link to={github}>
-            <img  className="project__link--github" src={GitHubIcon} alt="Github" />
-          </Link>
-
-          <Link to= {weblink}>
+          <a href={github}>
+            <img
+              className="project__link--github"
+              src={GitHubIcon}
+              alt="Github"
+            />
+          </a>
+            
+        <a href={weblink}>
             <img className="project__link--live" src={webLink} alt="web link" />
-          </Link>
+        </a>
         </div>
       </div>
     </div>
